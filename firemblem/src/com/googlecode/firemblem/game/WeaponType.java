3e7f1b97e512a0fb6b872f4b67125f97b090bfcc
nilaykumar07 <nilaykumar07@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-10-21 00:06:53 +0000

    Created test for Weapon serialization.

---
+++ firemblem/src/com/googlecode/firemblem/game/WeaponType.java
@@ -0,0 +1,15 @@
+package com.googlecode.firemblem.game;
+
+public enum WeaponType {
+
+	SWORD,
+	LANCE,
+	AXE,
+	BOW,
+	ANIMA,
+	LIGHT,
+	DARK,
+	MONSTER,
+	STAFF
+	
+}
---
+++ firemblem/src/com/googlecode/firemblem/test/WeaponSerializationTest.java
@@ -0,0 +1,30 @@
+package com.googlecode.firemblem.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.googlecode.firemblem.data.WeaponSerializer;
+
+/** Test this program with input in the form of "Fenrir.w"
+ * 
+ * @author Nilay Kumar
+ *
+ */
+public class WeaponSerializationTest {
+
+	public static void main(String[] args) {
+		WeaponSerializer ws = new WeaponSerializer();
+		try {
+			ws.load("src/com/googlecode/firemblem/data/weapons.txt");
+			ws.write("src/com/googlecode/firemblem/data/weapons/");
+			ws.read("src/com/googlecode/firemblem/data/weapons/");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
