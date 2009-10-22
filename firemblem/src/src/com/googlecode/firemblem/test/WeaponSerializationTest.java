93f8722bb845af9cc68280f29925826ae8b2037f
Cycofactory <Cycofactory@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-10-22 00:57:47 +0000

    Added the Item classes (and a seemingly useless interface....tell me if I did it wrong.)

--- firemblem/src/src/com/googlecode/firemblem/test/WeaponSerializationTest.java
+++ firemblem/src/src/com/googlecode/firemblem/test/WeaponSerializationTest.java
@@ -12,7 +12,7 @@ import com.googlecode.firemblem.data.WeaponSerializer;
  */
 public class WeaponSerializationTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		WeaponSerializer ws = new WeaponSerializer();
 		try {
 			ws.load("src/com/googlecode/firemblem/data/weapons.txt");
