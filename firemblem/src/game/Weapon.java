98df05ee71d1280470425fd1f5545aee27a2ce8c
Xiagu.Zepheron <Xiagu.Zepheron@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-10-30 22:45:59 +0000

    Made fields public.

--- firemblem/src/game/Weapon.java
+++ firemblem/src/game/Weapon.java
@@ -10,15 +10,15 @@ public class Weapon implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -8648673134412314167L;
-	
-	private String name;
-	private WeaponType type;
-	private float hit;
-	private float crit;
-	private int attack;
-	private int weight;
-	private int durability;
-	private int range;
+
+	public String name;
+	public WeaponType type;
+	public float hit;
+	public float crit;
+	public int attack;
+	public int weight;
+	public int durability;
+	public int range;
 
 	/**
 	 * @param name
@@ -30,7 +30,7 @@ public class Weapon implements Serializable {
 	 * @param durability
 	 * @param range
 	 */
-	//Brian Clanton made this temporarily public
+	// Brian Clanton made this temporarily public
 	public Weapon(String name, WeaponType type, float hit, float crit,
 			int attack, int weight, int durability, int range) {
 		super();
@@ -54,73 +54,17 @@ public class Weapon implements Serializable {
 					range);
 	}
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @return the type
-	 */
-	public WeaponType getType() {
-		return type;
-	}
-
-	/**
-	 * @return the hit
-	 */
-	public float getHit() {
-		return hit;
-	}
-
-	/**
-	 * @return the crit
-	 */
-	public float getCrit() {
-		return crit;
-	}
-
-	/**
-	 * @return the attack
-	 */
-	public int getAttack() {
-		return attack;
-	}
-
-	/**
-	 * @return the weight
-	 */
-	public int getWeight() {
-		return weight;
-	}
-
-	/**
-	 * @return the durability
-	 */
-	public int getDurability() {
-		return durability;
-	}
-
-	/**
-	 * @return the range
-	 */
-	public int getRange() {
-		return range;
-	}
-
 	@Override
 	public String toString() {
 		String s = "";
-		s += "Name: " + name + "\n";
-		s += "Type: " + type.toString() + "\n";
-		s += "Attack: " + attack + "\n";
-		s += "Hit: " + hit + "\n";
-		s += "Critical: " + crit + "\n";
-		s += "Weight: " + weight + "\n";
-		s += "Durability: " + durability + "\n";
-		s += "Range: " + range;
+		s += "Name: " + name;
+		s += "\nType: " + type.toString();
+		s += "\nAttack: " + attack;
+		s += "\nHit: " + hit;
+		s += "\nCritical: " + crit;
+		s += "\nWeight: " + weight;
+		s += "\nDurability: " + durability;
+		s += "\nRange: " + range;
 		return s;
 	}
 }
