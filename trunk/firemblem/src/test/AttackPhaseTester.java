7ae633c83f69bf5a5b6dc9fd4fa6ad647db00459
brian.clanton <brian.clanton@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-11-07 18:18:50 +0000

    Made changes to my Attack Phase Tester. You can now choose which type of characters you would like to battle while the program is running. Fixed layout manager problem so pictures appear correctly now. Note: At this stage, every character is initialized with an "iron" weapon except for cleric, mage, and lord.

--- trunk/firemblem/src/test/AttackPhaseTester.java
+++ trunk/firemblem/src/test/AttackPhaseTester.java
@@ -10,9 +10,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -21,23 +25,27 @@ import javax.swing.SwingConstants;
 import game.Character;
 import game.Weapon;
 import data.CharacterType;
+import data.StatType;
 import data.TerrainType;
 import data.WeaponType;
 import game.Map;
 
 public class AttackPhaseTester extends JFrame{
 
-	private JPanel pane, stats, b;
+	private JPanel pane, stats, b, t;
 	private JLabel img1, img2, name1 = new JLabel(),
 		name2 = new JLabel(), weapon1 = new JLabel(), weapon2 = new JLabel(), 
 		hp1 = new JLabel(), hp2  = new JLabel(), ter1 = new JLabel(), ter2 = new JLabel();
-	private Character one, two;
-	private JFrame container;
+	public Character one, two;
 	private JButton start, reset;
-	private JTextArea log = new JTextArea();
+	private JTextArea log = new JTextArea(10, 500);
 	private boolean characterOneTurn = true;
 	private int max1, max2;
 	private JScrollPane l;
+	private JMenu menu;
+	private JMenuBar menuBar;
+	private JMenuItem settings;
+	
 	
 	class AttackButtonListener implements ActionListener{
 		public void actionPerformed(ActionEvent e){
@@ -45,27 +53,29 @@ public class AttackPhaseTester extends JFrame{
 			if(characterOneTurn){
 				a += one.attack(two);
 				a += two.attack(one);
-				if(one.doubleAttack(two) && two.hp > 0){
-					a += "Double Attack\n";						
+				if(one.doubleAttack(two) && one.status != StatType.DEAD && two.status != StatType.DEAD){
+					a += one.name + "gets a Double Attack\n";						
 					a += one.attack(two);
 				}//end if
+				start.setText(two.name + "'s Attack");
 			}//end if
 			else{
 				a += two.attack(one);
 				a += one.attack(two);
-				if(two.doubleAttack(one) && one.hp > 0){
-					a += "Double Attack\n";
+				if(two.doubleAttack(one) && one.status != StatType.DEAD && two.status != StatType.DEAD){
+					a += two.name + " gets a Double Attack\n";
 					a += two.attack(one);
 				}//end if
+				start.setText(one.name + "'s Attack");
 			}//end else
 			log.setText(log.getText() + "\n" + a);
-			if(one.hp < 1){
+			if(one.status == StatType.DEAD){
 				hp1.setText("DEAD");
 				start.setEnabled(false);
 			}//end if
 			else
 				hp1.setText("" + one.hp + " / " + max1);
-			if(two.hp < 1){
+			if(two.status == StatType.DEAD){
 				hp2.setText("DEAD");
 				start.setEnabled(false);
 			}//end if
@@ -81,25 +91,45 @@ public class AttackPhaseTester extends JFrame{
 		}
 	}//end ResetButtonListener
 	
+	class CharacterSettingsListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			dispose();
+			CharacterChooser c = new CharacterChooser();
+		}
+	}//end CharacterSetttingsListener
+	
 	public static Map g = new Map(4,4);
 	
 	public static void main(String[] args) {
-		AttackPhaseTester test = new AttackPhaseTester();
 		JFrame.setDefaultLookAndFeelDecorated(true);
+		CharacterChooser a = new CharacterChooser();
 	}//end main
 	
-	public AttackPhaseTester(){
-		pane = new JPanel(new GridLayout(3, 1));
+	public AttackPhaseTester(Character c1, Character c2){
+		super("Attack Phase Tester - Brian Clanton 10/30/09");
+		pane = new JPanel();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.PAGE_AXIS));
 		stats = new JPanel(new GridLayout(5, 2));
 		b = new JPanel(new FlowLayout());
 		
 		g.changeAll(TerrainType.GRASS);
 		g.changeSpace(0, 0, TerrainType.MTNS);
 		
-		start = new JButton("Attack");
+		one = c1;
+		two = c2;
+		
+		start = new JButton();
 		start.addActionListener(new AttackButtonListener());
 		reset = new JButton("Reset");
 		reset.addActionListener(new ResetButtonListener());
+		
+		settings = new JMenuItem("Change Characters...");
+		settings.addActionListener(new CharacterSettingsListener());
+		menu = new JMenu("Settings");
+		menu.add(settings);
+		menuBar = new JMenuBar();
+		menuBar.add(menu);
+		setJMenuBar(menuBar);
 		
 		initialize();
 		
@@ -131,26 +161,29 @@ public class AttackPhaseTester extends JFrame{
 		b.add(start);
 		b.add(reset);
 		
-		l = new JScrollPane(log);
+		l = new JScrollPane(log, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		/*t = new JPanel();
+		t.add(l);
+		*/
 		
 		pane.add(stats);
 		pane.add(b);
 		pane.add(l);
 		
-		container = new JFrame("Attack Phase Tester - Brian Clanton 10/30/09");
-		container.setContentPane(pane);
-		container.setVisible(true);
-		container.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		container.setSize(500, 700);
-	}
+		setContentPane(pane);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(500, 700);
+		setVisible(true);
+		JFrame.setDefaultLookAndFeelDecorated(true);
+	}//end constructor
 	
 	private void initialize(){
-		one = new Character(CharacterType.MYR, 0, 0);
-		two = new Character(CharacterType.FGT, 0, 1);
-		one.activeWeapon = new Weapon("Iron Sword", WeaponType.SWORD, 5, 90, 0, 5, 45, 1);
-		two.activeWeapon = new Weapon("Iron Axe", WeaponType.AXE, 8, 75, 0, 10, 45, 1);
-		
 		start.setEnabled(true);
+		start.setText(one.name + "'s Attack");
+		one = new Character(one.type, 0, 0);
+		two = new Character(two.type, 0, 1);
+		one.activeWeapon = CharacterChooser.getActiveWeapon(one);
+		two.activeWeapon = CharacterChooser.getActiveWeapon(two);
 		weapon1.setText(one.activeWeapon.name);
 		weapon2.setText(two.activeWeapon.name);
 		max1 = one.hp;
@@ -162,7 +195,6 @@ public class AttackPhaseTester extends JFrame{
 		ter1.setText(g.grid[one.position.x][one.position.y].name);
 		ter2.setText(g.grid[two.position.x][two.position.y].name);
 		log.setText("");
-	}//end initialize
-	
+	}//end initialize	
 
 }
