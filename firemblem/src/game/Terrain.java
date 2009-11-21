fff8b51e8c7be6f8cb2cb07ac0bded39aa248f3a
Xiagu.Zepheron <Xiagu.Zepheron@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-11-21 22:11:58 +0000

    Added tasks to Terrain. Added the map tiles to the repository.

--- firemblem/src/game/Terrain.java
+++ firemblem/src/game/Terrain.java
@@ -1,15 +1,20 @@
 package game;
 
-import java.util.TreeMap;
-
 import data.TerrainType;
 
-//Brian Clanton
 //10/21/09
-//Forgot to add hp effects, will add later.
 
-public class Terrain{
-	//evade boost is a percentage
+// TODO: Add HP effects
+// TODO: Give Terrains associated images, groups of possible images, or animations (ex. water tiles).
+
+/**
+ * @author Brian Clanton
+ */
+public class Terrain {
+
+	/**
+	 * The percent increase in evasion.
+	 */
 	public int evadeBoost;
 	public int defenseBoost;
 	public int moveEffect;
@@ -17,119 +22,127 @@ public class Terrain{
 	public boolean occupied;
 	public TerrainType type;
 
-	public Terrain(){
-		evadeBoost=0;
-		defenseBoost=0;
-		moveEffect=0;
-		name="?";
-		occupied=false;
+	public Terrain() {
+		evadeBoost = 0;
+		defenseBoost = 0;
+		moveEffect = 0;
+		name = "?";
+		occupied = false;
 		type = TerrainType.GRASS;
-	}//end default constructor
+	}// end default constructor
 
-	public Terrain(TerrainType t){
+	/**
+	 * Constructs a Terrain object with the correct attributes for the given
+	 * TerrainType.
+	 * 
+	 * @param t
+	 *            the TerrainType of the new Terrain
+	 */
+	public Terrain(TerrainType t) {
 		type = t;
-		switch(t){
-			case WOODS:
-				evadeBoost = 20;
-				defenseBoost = 1;
-				//moveEffect = 
-				name = "Forest";
+		switch (t) {
+		case WOODS:
+			evadeBoost = 20;
+			defenseBoost = 1;
+			// moveEffect =
+			name = "Forest";
 			break;
 
-			case SAND:
-				evadeBoost = 5;
-				defenseBoost = 0;
-				name = "Sand";
+		case SAND:
+			evadeBoost = 5;
+			defenseBoost = 0;
+			name = "Sand";
 			break;
 
-			case HIGHMTNS:
-				evadeBoost = 40;
-				defenseBoost = 0;
-				name = "High Mountains";
+		case HIGHMTNS:
+			evadeBoost = 40;
+			defenseBoost = 0;
+			name = "High Mountains";
 			break;
 
-			case MTNS:
-				evadeBoost = 30;
-				defenseBoost = 1;
-				name = "Mountains";
+		case MTNS:
+			evadeBoost = 30;
+			defenseBoost = 1;
+			name = "Mountains";
 			break;
 
-			case PILLAR:
-				evadeBoost = 20;
-				defenseBoost = 1;
-				name = "Pillar";
-			break;
-			
-			case THRONE:
-				evadeBoost = 30;
-				defenseBoost = 3;
-				name = "Throne";
+		case PILLAR:
+			evadeBoost = 20;
+			defenseBoost = 1;
+			name = "Pillar";
 			break;
 
-			case GATE:
-				evadeBoost = 30;
-				defenseBoost = 3;
-				name = "Castle Gate";
-			break;
-			
-			case SEA:
-				evadeBoost = 10;
-				defenseBoost = 0;
-				name = "Sea";
+		case THRONE:
+			evadeBoost = 30;
+			defenseBoost = 3;
+			name = "Throne";
 			break;
 
-			case VILLAGE:
-				evadeBoost = 10;
-				defenseBoost = 0;
-				name = "Village";
+		case GATE:
+			evadeBoost = 30;
+			defenseBoost = 3;
+			name = "Castle Gate";
 			break;
 
-			case TILE:
-				evadeBoost = 0;
-				defenseBoost = 0;
-				name = "Tile";
+		case SEA:
+			evadeBoost = 10;
+			defenseBoost = 0;
+			name = "Sea";
 			break;
 
-			case GRASS:
-				evadeBoost = 0;
-				defenseBoost = 0;
-				name = "Grass";
+		case VILLAGE:
+			evadeBoost = 10;
+			defenseBoost = 0;
+			name = "Village";
 			break;
-		}//end switch
-		occupied=false;
-		moveEffect=0;
-	}//end constructor
+
+		case TILE:
+			evadeBoost = 0;
+			defenseBoost = 0;
+			name = "Tile";
+			break;
+
+		case GRASS:
+			evadeBoost = 0;
+			defenseBoost = 0;
+			name = "Grass";
+			break;
+		}// end switch
+		occupied = false;
+		moveEffect = 0;
+	}// end constructor
 
 	@Override
 	public String toString() {
 		return "" + name + "\ndefenseBoost=" + defenseBoost + "\nevadeBoost="
-				+ evadeBoost + "\nmoveEffect=" + moveEffect + "\noccupied=" + occupied;
-	}//end toString
-	
-	/**
-	 * this method gets the evade bonus given a characters base stat.
-	 * @param baseStat
-	 * @return evade bonus
-	 */
-	public int getEvadeBonus(int baseStat){
-		return (int) Math.floor(baseStat + evadeBoost/100 * baseStat);
-	}//end getEvadeBonus
+				+ evadeBoost + "\nmoveEffect=" + moveEffect + "\noccupied="
+				+ occupied;
+	}// end toString
 
 	/**
-	 * this method gets the defense bonus given a characters base stat.
+	 * This method gets the evade bonus given a characters base stat.
+	 * 
 	 * @param baseStat
-	 * @return defense bonus
+	 *            the original base stat
+	 * @return the modified base stat
 	 */
-	public int getDefenseBonus(int baseStat){
+	public int getEvadeBonus(int baseStat) {
+		return (int) Math.floor(baseStat + evadeBoost / 100 * baseStat);
+	}// end getEvadeBonus
+
+	/**
+	 * This method gets the defense bonus given a characters base stat.
+	 * 
+	 * @param baseStat
+	 *            the original base stat
+	 * @return the modified base stat
+	 */
+	public int getDefenseBonus(int baseStat) {
 		return baseStat + defenseBoost;
-	}//end getDefenseBonus
-	
-	
-	/*
-	 * Methods to be created:
-	 * getMoveDecrease
-	 * getHpIncrease
-	 */
+	}// end getDefenseBonus
 
-}//end class Terrain
+	// TODO: Write getMoveDecrease
+	// TODO: Write getHpIncrease
+
+}// end class Terrain
 
