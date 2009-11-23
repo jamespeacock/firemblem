688538588f0dd1c53cb0d1295f88e5c703604a85
brian.clanton <brian.clanton@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-11-23 23:38:22 +0000

    started changes to character type to incorporate usable weapons.

--- trunk/firemblem/src/data/CharacterType.java
+++ trunk/firemblem/src/data/CharacterType.java
@@ -1,9 +1,14 @@
 package data;
+
+import java.util.HashSet;
+
 /*
  * Brian Clanton
  * 10/24/09
  */
 public enum CharacterType {
 	MYR, LORD, KNG, FGT, ARCH, MAGE, CLR, RCRT, CVLR, BRSKR, PLDN, GNRL, MCNRY, PKNG, JNYMN, PUPIL, HERO,
-	TRBDR, PRT, FKNG, THF
+	TRBDR, PRT, FKNG, THF;
+	
+	public HashSet usableWeapons;
 }
