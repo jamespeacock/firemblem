25a9cca9382a6a712bae9ec7c2628c2f3c72c8d7
brian.clanton <brian.clanton@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-11-24 00:19:33 +0000

    Made some modifications to CharacterType for the usable weapons. Also added toString methods and name variables to other Enum classes.

--- firemblem/src/data/CharacterType.java
+++ firemblem/src/data/CharacterType.java
@@ -1,14 +1,57 @@
 package data;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashSet;
+import java.util.Scanner;
 
 /*
  * Brian Clanton
  * 10/24/09
  */
+
 public enum CharacterType {
-	MYR, LORD, KNG, FGT, ARCH, MAGE, CLR, RCRT, CVLR, BRSKR, PLDN, GNRL, MCNRY, PKNG, JNYMN, PUPIL, HERO,
-	TRBDR, PRT, FKNG, THF;
+	MYR("Myrmidon"), LORD("Lord"), KNG("Knight"), FGT("Fighter"), ARCH("Archer"), MAGE("Mage"), 
+	CLR("Cleric"), RCRT("Recruit"), CVLR("Cavalier"), BRSKR("Beserker"), PLDN("Paladin"), 
+	GNRL("General"), MCNRY("Mercenary"), PKNG("Pegasus Knight"), JNYMN("Journeyman"),
+	PUPIL("Pupil"), HERO("Hero"), TRBDR("Troubador"), PRT("Pirate"), FKNG("Falcoknight"),
+	THF("Theif");
 	
-	public HashSet usableWeapons;
+	public HashSet<WeaponType> usableWeapons;
+	public String name;
+	
+	static String [] textFile = new String [10];
+	static boolean textFileRead = false;
+	
+	private CharacterType(String a){
+		ParseMethods.initializeWeaponSearchMap();
+		name = a;
+		usableWeapons = new HashSet<WeaponType> ();
+		String [] f = read();
+		String line;
+		for(int i = 0; i < f.length; i++){
+			line = f[i];
+			if(line.contains(name) || line.contains("all"))
+				usableWeapons.add(ParseMethods.toWeaponType(line.substring(0, line.indexOf(':'))));
+		}//end for
+	}//end constructor
+	
+	private static String [] read(){
+		if(!textFileRead){
+			try{
+				Scanner reader = new Scanner(new FileReader("src/data/usableweapons.txt"));
+				for(int i = 0; i < 10; i++)
+					textFile[i] = reader.nextLine().trim();
+				textFileRead = true;
+			}
+			catch(FileNotFoundException e){
+				
+			}
+		}//end if
+		return textFile;
+	}//end read
+	
+	public String toString(){
+		return name;
+	}
 }
