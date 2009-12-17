6b47ac5d214a216250dd394efe1bd15c136d40ce
Xiagu.Zepheron <Xiagu.Zepheron@19ff8a98-bdd5-11de-ac70-2fe2a02b5c02>
2009-12-17 02:05:15 +0000

    Updated Map.

--- trunk/firemblem/src/game/Map.java
+++ trunk/firemblem/src/game/Map.java
@@ -1,20 +1,26 @@
 package game;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import data.TerrainType;
 
 public class Map {
     public Terrain[][] grid;
+    public HashSet<Character> units;
 
     public Map() {
 	grid = null;
+	units = new HashSet<Character>();
     }// end default constructor
 
-    public Map(int x, int y) {
-	grid = new Terrain[x][y];
+    public Map(int width, int height) {
+	grid = new Terrain[height][width];
+	units = new HashSet<Character>();
     }// end constructor
 
-    public void changeSpace(int x, int y, TerrainType t) {
-	grid[x][y] = new Terrain(t);
+    public void changeSpace(int row, int col, TerrainType t) {
+	grid[row][col] = new Terrain(t);
     }// end changeSpace
 
     public void changeRow(int row, TerrainType t) {
@@ -32,6 +38,19 @@ public class Map {
 	    for (int j = 0; j < grid[i].length; j++)
 		grid[i][j] = new Terrain(t);
     }// end changeAll
+
+    /**
+     * Resizes the Map to newWidth by newHeight
+     * 
+     * @param newWidth
+     * @param newHeight
+     */
+    public void resize(int newWidth, int newHeight) {
+	Arrays.copyOf(grid, newHeight);
+	for (int i = 0; i < grid.length; i++) {
+	    grid[i] = Arrays.copyOf(grid[i], newWidth);
+	}
+    }
 
     public void print() {
 	for (int i = 0; i < grid.length; i++) {
